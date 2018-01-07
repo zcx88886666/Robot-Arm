96d3ad44a358d1517b8aba62553b8296626a469d
zcx88886666 <zcx88886666@gmail.com>
2018-01-07 03:53:21 -0600

    environment and viewer initialization by pyglet

--- env.py
+++ env.py
@@ -1,5 +1,10 @@
+import numpy as np
+import pyglet
+
 
 class ArmEnv(object):
+   viewer = None
+   
    def __init__(self):
        pass
 
@@ -10,4 +15,24 @@ class ArmEnv(object):
        pass
 
    def render(self):
+       if self.viewer is None:
+          self.viewer = Viewer()
+       self.viewer.render()
+
+class Viewer(pyglet.window.Window):
+   def __init__(self):
        pass
+
+   def render(self):
+       pass
+
+   def on_draw(self):
+       pass
+
+   def _update_arm(self):
+       pass
+
+if __name__ == "__main__":
+   env = ArmEnv()
+   while True:
+       env.render()
