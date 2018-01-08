3faa064178f1a67e484cc6a6adeadbbcdc52ce57
zcx88886666 <zcx88886666@gmail.com>
2018-01-07 18:22:09 -0600

    The initial drawing the point the robot arm

--- env.py
+++ env.py
@@ -20,14 +20,45 @@ class ArmEnv(object):
        self.viewer.render()
 
 class Viewer(pyglet.window.Window):
+   bar_thc = 5
+
    def __init__(self):
-       pass
+       # vsync=False to not use the monitor FPS, we can speed up training
+        super(Viewer, self).__init__(width=400, height=400, resizable=False, caption='Arm', vsync=False)
+        pyglet.gl.glClearColor(1, 1, 1, 1)
+
+        self.batch = pyglet.graphics.Batch()    # display whole batch at once
+        self.point = self.batch.add(
+            4, pyglet.gl.GL_QUADS, None,    # 4 corners
+            ('v2f', [50, 50,                # location
+                     50, 100,
+                     100, 100,
+                     100, 50]),
+            ('c3B', (86, 109, 249) * 4))    # color
+        self.arm1 = self.batch.add(
+            4, pyglet.gl.GL_QUADS, None,
+            ('v2f', [250, 250,                # location
+                     250, 300,
+                     260, 300,
+                     260, 250]),
+            ('c3B', (249, 86, 86) * 4,))    # color
+        self.arm2 = self.batch.add(
+            4, pyglet.gl.GL_QUADS, None,
+            ('v2f', [100, 150,              # location
+                     100, 160,
+                     200, 160,
+                     200, 150]), ('c3B', (249, 86, 86) * 4,))
 
    def render(self):
-       pass
+       self._update_arm()
+       self.switch_to()
+       self.dispatch_events()
+       self.dispatch_event('on_draw')
+       self.flip()
 
    def on_draw(self):
-       pass
+       self.clear()
+       self.batch.draw()
 
    def _update_arm(self):
        pass
